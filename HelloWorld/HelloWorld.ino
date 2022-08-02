54459019a6ec2c5ed3b55fa2a3b578b0a4b9d59e
Jens True <jens.chr.true@gmail.com>
2022-08-02 11:24:14 +0000

    Terminate simulation with sleep

--- HelloWorld/HelloWorld.ino
+++ HelloWorld/HelloWorld.ino
@@ -1,14 +1,20 @@
+#include <avr/sleep.h>
 void setup()
 {
+	Serial.begin(115200);
 	pinMode(13, OUTPUT);
 }
 
 void loop()
 {
+	Serial.println("Hello World");
 	digitalWrite(13, HIGH);
     delay(100);
     digitalWrite(13, LOW);
 	delay(100);
+	noInterrupts();
+   	sleep_enable();
+    sleep_cpu();
 }
 
 
